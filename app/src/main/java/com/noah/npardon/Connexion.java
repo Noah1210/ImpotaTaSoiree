cbc48d22fda5ebd2315b0b6d21f6f237f49d3203
Noah1210 <nounonoc@gmail.com>
2022-11-21 17:01:26 +0100

    add Connexion layout

---
+++ app/src/main/java/com/noah/npardon/Connexion.java
@@ -0,0 +1,16 @@
+package com.noah.npardon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.npardon.R;
+
+public class Connexion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connexion);
+    }
+}
